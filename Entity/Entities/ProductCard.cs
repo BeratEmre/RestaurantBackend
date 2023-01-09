d4bab1db76e7f72b3f49ad69e7b1b99e13205f4f
Berat <berat2emre@gmail.com>
2023-01-09 23:07:02 +0300

    a

---
+++ Entity/Entities/ProductCard.cs
@@ -0,0 +1,12 @@
+﻿namespace Entity.Entities
+{
+    public class ProductCard
+    {
+        public int ProductId { get; set; }
+        public byte ProductType { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string ImgUrl { get; set; }
+    }
+}
